1e56c7c9750f5609169639126199c417a1e65fc4
Surur Khan <surur.rkhan@gmail.com>
2022-05-09 11:07:28 -0400

    Game is playable but only results box updates

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-let playerWins = false;
-let computerWins = false;
 let playerScore = 0;
 let computerScore = 0;
-let gameon = true;
-let playerPoints = document.querySelector('.playerScore');
+let playerPoints = document.querySelector('#playerScore');
 let options = document.querySelectorAll('div.options button');
-let computerPoints = document.querySelector('.computerScore');
+let computerPoints = document.querySelector('#computerScore');
 let roundResults = document.querySelector('.result-box');
 
 options.forEach(button => { button.addEventListener('click', getPlayerChoice) });
@@ -16,65 +13,49 @@ function computerPlay() {
     return randomChoice;
 }
 
+function getPlayerChoice(e) {
+    getRef = e.target.textContent;
+    playerChoice = getRef.toLowerCase();
+    playRound(playerChoice, computerPlay());
+}
+
 function playRound(playerSelection, computerSelection) {
-    // validate user input
-    // while (true) {
-    //     playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    //     if (playerSelection == "rock" ||
-    //         playerSelection == "paper" ||
-    //         playerSelection == "scissors") {
-    //         break;
-    //     } else {
-    //         alert("Please enter rock, paper, or scissors.");
-    //     }
-    // }
     computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
-        playerWins = false;
-        computerWins = false;
+        roundResults.textContent = "Tie!"
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        playerWins = true;
-        computerWins = false;
+        playerPoints.textContent == ++playerScore;
+        roundResults.textContent = "You win this round! :)";
     } else {
-        playerWins = false;
-        computerWins = true;
+        computerPoints.textContent == ++computerScore;
+        roundResults.textContent = "You lost this round! :(";
+    }
+    checkWinner();
+}
 
+const winnerResults = {
+    computer: ["You lost!", 'red'],
+    player: ["You won!", 'green']
+}
+
+function checkWinner() {
+    if (computerScore === 5) {
+        roundResults.textContent = winnerResults["computer"][0];
+        roundResults.style.color = winnerResults["computer"][1];
+        endGame();
+    } else if (playerScore === 5) {
+        roundResults.textContent = winnerResults["player"][0];
+        roundResults.style.color = winnerResults["player"][1];
+        endGame();
+    } else {
+        getPlayerChoice();
     }
 }
 
-function game() {
-    while (gameon) {
-        round = playRound();
-        if (playerWins) {
-            playerPoints.textContent == ++playerScore;
-            roundResults.textContent = "You win this round! :)";
-            //console.log("You win this round! :)")
-        } else if (computerWins) {
-            computerPoints.textContent == ++computerScore;
-            roundResults.textContent = "You lost this round! :(";
-            //console.log("You lost this round! :(")
-        } else {
-            roundResults.textContent = "Tie!"
-        }
-        if (playerScore === 5) {
-            gameon = false;
-            return `You won the game!
-                    Score was Player ${playerScore} : Opponent ${computerScore}`;
-
-        } else if (computerScore === 5) {
-            gameon = false;
-            return `You lost the game!
-                    Score was Player ${playerScore} : Opponent ${computerScore}`;
-
-        }
-        console.log(`Player: ${playerScore} Computer: ${computerScore}`);
-    }
-}
-
-function getPlayerChoice(e) {
-    let playerSelection = (e.target.id);
-    playerChoice = e.target.textContent;
-    game(playerSelection, computerPlay());
+function endGame() {
+    options.forEach(button => {
+        button.removeEventListener('click', getPlayerChoice);
+    })
 }
